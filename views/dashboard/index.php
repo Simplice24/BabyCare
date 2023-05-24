d55cbddd65909b865bcbd4958f1ce1634ac0525f
Simplice NIYONZIMA <89002414+Simplice24@users.noreply.github.com>
2023-05-24 12:38:49 -0800

    first commit

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -82,10 +82,6 @@ use yii\helpers\Html;
         <!-- Sidebar navigation-->
         <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
           <ul id="sidebarnav">
-            <li class="nav-small-cap">
-              <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-              <span class="hide-menu">Home</span>
-            </li>
             <li class="sidebar-item">
               <a class="sidebar-link" href="./index.html" aria-expanded="false">
                 <span>
