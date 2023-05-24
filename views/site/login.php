f9f7bb94531b50b6de01aac9e671029cdcfeb738
Simplice NIYONZIMA <89002414+Simplice24@users.noreply.github.com>
2023-05-24 12:22:48 -0800

    first commit

--- views/site/login.php
+++ views/site/login.php
@@ -30,7 +30,7 @@ use yii\widgets\ActiveForm;
           <div class="col-md-8 col-lg-6 col-xxl-3">
             <div class="card mb-0">
               <div class="card-body">
-                <a href="" class="text-nowrap logo-img text-center d-block py-3 w-100">
+                <a href="<?= Yii::$app->urlManager->createUrl(['site/index']) ?>" class="text-nowrap logo-img text-center d-block py-3 w-100">
                   <img src="images/favicon.png" width="30" alt="">
                 </a>
                 <p class="text-center">Your Social Campaigns</p>
