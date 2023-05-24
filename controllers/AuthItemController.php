ad036dab28f7dc5886c2e808a2a4404dfc71c61c
Simplice NIYONZIMA <89002414+Simplice24@users.noreply.github.com>
2023-05-23 19:18:43 -0800

    first commit

--- controllers/AuthItemController.php
+++ controllers/AuthItemController.php
@@ -104,6 +104,8 @@ class AuthItemController extends Controller
         ]);
     }
 
+    
+
     /**
      * Updates an existing AuthItem model.
      * If update is successful, the browser will be redirected to the 'view' page.
