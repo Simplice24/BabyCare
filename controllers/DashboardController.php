abd7068527792ab362a525465b6b6fd95be30ee6
Simplice NIYONZIMA <89002414+Simplice24@users.noreply.github.com>
2023-05-26 10:31:08 -0800

    first commit

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -1,9 +1,31 @@
 <?php
 
 namespace app\controllers;
+use yii\filters\AccessControl;
+use Yii;
 
 class DashboardController extends \yii\web\Controller
 {
+
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'actions' => ['index'],
+                        'roles' => ['@'], // '@' represents authenticated users
+                    ],
+                ],
+                'denyCallback' => function ($rule, $action) {
+                    return Yii::$app->response->redirect(['site/login']);
+                },
+            ],
+        ];
+    }
+
     public function actionIndex()
     {
         return $this->render('index');
