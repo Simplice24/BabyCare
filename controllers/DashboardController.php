12fc23cb5056d55c706b489028fc6813a8d9e0e0
Simplice NIYONZIMA <89002414+Simplice24@users.noreply.github.com>
2023-06-05 22:00:43 -0800

    first commit

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -36,8 +36,33 @@ class DashboardController extends \yii\web\Controller
         $bookings = Bookings::find()->count(); 
         $parents= User::find()->where(['role' => 'Parent'])->count();
         $babysitters= User::find()->where(['role' => 'Babysitter'])->count();
+
+        // Query to find the most frequent languages spoken
+        $mostFrequentLanguages = Bookings::find()
+        ->select('languages_spoken')
+        ->groupBy('languages_spoken')
+        ->orderBy(['COUNT(*)' => SORT_DESC])
+        ->limit(1)
+        ->scalar();
+
+        // Query to find the most frequent gender
+        $mostFrequentGender = Bookings::find()
+        ->select('gender')
+        ->groupBy('gender')
+        ->orderBy(['COUNT(*)' => SORT_DESC])
+        ->limit(1)
+        ->scalar();
+
+        // Query to find the most frequent babysitter age range
+        $mostFrequentAgeRange = Bookings::find()
+        ->select('babysitter_age_range')
+        ->groupBy('babysitter_age_range')
+        ->orderBy(['COUNT(*)' => SORT_DESC])
+        ->limit(1)
+        ->scalar();
+
         return $this->render('index',['userProfileImage' => $userProfileImage, 'babysitters' => $babysitters,'parents' => $parents,
-        'bookings' => $bookings]);
+        'bookings' => $bookings,'mostFrequentAgeRange' => $mostFrequentAgeRange, 'mostFrequentGender' => $mostFrequentGender, 'mostFrequentLanguages' => $mostFrequentLanguages]);
     }
 
 }
--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -485,60 +485,32 @@ use yii\helpers\Html;
             <div class="card w-100">
               <div class="card-body p-4">
                 <div class="mb-4">
-                  <h5 class="card-title fw-semibold">Recent Notifications</h5>
+                  <h5 class="card-title fw-semibold">Booking Trendings</h5>
                 </div>
                 <ul class="timeline-widget mb-0 position-relative mb-n5">
                   <li class="timeline-item d-flex position-relative overflow-hidden">
-                    <div class="timeline-time text-dark flex-shrink-0 text-end">09:30</div>
+                    <div class="timeline-time text-dark flex-shrink-0 text-end">Language</div>
                     <div class="timeline-badge-wrap d-flex flex-column align-items-center">
                       <span class="timeline-badge border-2 border border-primary flex-shrink-0 my-8"></span>
                       <span class="timeline-badge-border d-block flex-shrink-0"></span>
                     </div>
-                    <div class="timeline-desc fs-3 text-dark mt-n1">Payment received from John Doe of $385.90</div>
+                    <div class="timeline-desc fs-3 text-dark mt-n1"><?php echo $mostFrequentLanguages; ?></div>
                   </li>
                   <li class="timeline-item d-flex position-relative overflow-hidden">
-                    <div class="timeline-time text-dark flex-shrink-0 text-end">10:00 am</div>
+                    <div class="timeline-time text-dark flex-shrink-0 text-end">Gender</div>
                     <div class="timeline-badge-wrap d-flex flex-column align-items-center">
-                      <span class="timeline-badge border-2 border border-info flex-shrink-0 my-8"></span>
+                      <span class="timeline-badge border-2 border border-primary flex-shrink-0 my-8"></span>
                       <span class="timeline-badge-border d-block flex-shrink-0"></span>
                     </div>
-                    <div class="timeline-desc fs-3 text-dark mt-n1 fw-semibold">New sale recorded <a
-                        href="javascript:void(0)" class="text-primary d-block fw-normal">#ML-3467</a>
-                    </div>
+                    <div class="timeline-desc fs-3 text-dark mt-n1"><?php echo $mostFrequentGender; ?></div>
                   </li>
                   <li class="timeline-item d-flex position-relative overflow-hidden">
-                    <div class="timeline-time text-dark flex-shrink-0 text-end">12:00 am</div>
+                    <div class="timeline-time text-dark flex-shrink-0 text-end">Age range</div>
                     <div class="timeline-badge-wrap d-flex flex-column align-items-center">
-                      <span class="timeline-badge border-2 border border-success flex-shrink-0 my-8"></span>
+                      <span class="timeline-badge border-2 border border-primary flex-shrink-0 my-8"></span>
                       <span class="timeline-badge-border d-block flex-shrink-0"></span>
                     </div>
-                    <div class="timeline-desc fs-3 text-dark mt-n1">Payment was made of $64.95 to Michael</div>
-                  </li>
-                  <li class="timeline-item d-flex position-relative overflow-hidden">
-                    <div class="timeline-time text-dark flex-shrink-0 text-end">09:30 am</div>
-                    <div class="timeline-badge-wrap d-flex flex-column align-items-center">
-                      <span class="timeline-badge border-2 border border-warning flex-shrink-0 my-8"></span>
-                      <span class="timeline-badge-border d-block flex-shrink-0"></span>
-                    </div>
-                    <div class="timeline-desc fs-3 text-dark mt-n1 fw-semibold">New sale recorded <a
-                        href="javascript:void(0)" class="text-primary d-block fw-normal">#ML-3467</a>
-                    </div>
-                  </li>
-                  <li class="timeline-item d-flex position-relative overflow-hidden">
-                    <div class="timeline-time text-dark flex-shrink-0 text-end">09:30 am</div>
-                    <div class="timeline-badge-wrap d-flex flex-column align-items-center">
-                      <span class="timeline-badge border-2 border border-danger flex-shrink-0 my-8"></span>
-                      <span class="timeline-badge-border d-block flex-shrink-0"></span>
-                    </div>
-                    <div class="timeline-desc fs-3 text-dark mt-n1 fw-semibold">New arrival recorded 
-                    </div>
-                  </li>
-                  <li class="timeline-item d-flex position-relative overflow-hidden">
-                    <div class="timeline-time text-dark flex-shrink-0 text-end">12:00 am</div>
-                    <div class="timeline-badge-wrap d-flex flex-column align-items-center">
-                      <span class="timeline-badge border-2 border border-success flex-shrink-0 my-8"></span>
-                    </div>
-                    <div class="timeline-desc fs-3 text-dark mt-n1">Payment Done</div>
+                    <div class="timeline-desc fs-3 text-dark mt-n1"><?php echo $mostFrequentAgeRange; ?></div>
                   </li>
                 </ul>
               </div>
