36092d56afc69fd1421b1524886fbd315e7ac459
Simplice NIYONZIMA <89002414+Simplice24@users.noreply.github.com>
2023-06-07 18:00:14 -0800

    first commit

--- views/auth-item/view.php
+++ views/auth-item/view.php
@@ -234,14 +234,6 @@ use yii\helpers\Url;
                       <i class="ti ti-user fs-6"></i>
                       <p class="mb-0 fs-3">My Profile</p>
                     </a>
-                    <!-- <a href="javascript:void(0)" class="d-flex align-items-center gap-2 dropdown-item">
-                      <i class="ti ti-mail fs-6"></i>
-                      <p class="mb-0 fs-3">My Account</p>
-                    </a>
-                    <a href="javascript:void(0)" class="d-flex align-items-center gap-2 dropdown-item">
-                      <i class="ti ti-list-check fs-6"></i>
-                      <p class="mb-0 fs-3">My Task</p>
-                    </a> -->
                     <?= Html::a('Logout', ['site/logout'], ['class' => 'btn btn-outline-primary mx-3 mt-2 d-block', 'data' => ['method' => 'post']]) ?>
                   </div>
                 </div>
