50eb8df51d3047494a7b644eae6c95ca7c7916b6
Simplice NIYONZIMA <89002414+Simplice24@users.noreply.github.com>
2023-06-21 18:07:14 -0800

    first commit

--- controllers/ProfileController.php
+++ controllers/ProfileController.php
@@ -12,51 +12,51 @@ class ProfileController extends \yii\web\Controller
    
 
     public function actionUsername()
-{
-    $userId = Yii::$app->user->id;
-    $newUsername = Yii::$app->request->post('new_username');
+    {
+        $userId = Yii::$app->user->id;
+        $newUsername = Yii::$app->request->post('new_username');
 
-    $rowsAffected = Yii::$app->db->createCommand()
-        ->update('user', ['username' => $newUsername], ['id' => $userId])
-        ->execute();
+        $rowsAffected = Yii::$app->db->createCommand()
+            ->update('user', ['username' => $newUsername], ['id' => $userId])
+            ->execute();
 
-    return $this->redirect(Yii::$app->request->referrer);
-}
-
-public function actionPassword()
-{
-    $userId = Yii::$app->user->id;
-    $currentPassword = Yii::$app->request->post('current_password');
-    $newPassword = Yii::$app->request->post('new_password');
-    $confirmPassword = Yii::$app->request->post('confirm_password');
-
-    $model = User::findOne($userId); // Assuming your User model is named "User"
-
-    if (!$model) {
-        throw new NotFoundHttpException('User not found.');
+        return $this->redirect('user/index');
     }
 
-    // Verify that the current password matches the user's actual password
-    if (!$model->validatePassword($currentPassword)) {
-        return $this->redirect(Yii::$app->request->referrer);
+    public function actionPassword()
+    {
+        $userId = Yii::$app->user->id;
+        $currentPassword = Yii::$app->request->post('current_password');
+        $newPassword = Yii::$app->request->post('new_password');
+        $confirmPassword = Yii::$app->request->post('confirm_password');
+
+        $model = User::findOne($userId); // Assuming your User model is named "User"
+
+        if (!$model) {
+            throw new NotFoundHttpException('User not found.');
+        }
+
+        // Verify that the current password matches the user's actual password
+        if (!$model->validatePassword($currentPassword)) {
+            return $this->redirect(Yii::$app->request->referrer);
+        }
+
+        // Validate the new password and confirm password fields
+        if ($newPassword !== $confirmPassword) {
+            return $this->redirect(Yii::$app->request->referrer);
+        }
+
+        // Generate a new password hash
+        $newPasswordHash = Yii::$app->security->generatePasswordHash($newPassword);
+
+        // Update the password_hash attribute in the user table
+        $rowsAffected = Yii::$app->db->createCommand()
+            ->update('user', ['password_hash' => $newPasswordHash], ['id' => $userId])
+            ->execute();
+
+
+        return $this->redirect('user/index');
     }
-
-    // Validate the new password and confirm password fields
-    if ($newPassword !== $confirmPassword) {
-        return $this->redirect(Yii::$app->request->referrer);
-    }
-
-    // Generate a new password hash
-    $newPasswordHash = Yii::$app->security->generatePasswordHash($newPassword);
-
-    // Update the password_hash attribute in the user table
-    $rowsAffected = Yii::$app->db->createCommand()
-        ->update('user', ['password_hash' => $newPasswordHash], ['id' => $userId])
-        ->execute();
-
-
-    return $this->redirect('user/index');
-}
 
     public function actionBio()
     {
@@ -87,7 +87,7 @@ public function actionPassword()
             
         }
         
-        return $this->redirect('user/profile');
+        return $this->redirect('user/index');
     }
 
 
